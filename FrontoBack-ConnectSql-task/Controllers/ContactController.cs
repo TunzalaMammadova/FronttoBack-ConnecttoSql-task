047e9f9410e62354e020335271e9d5cbfaba028d
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-04-28 01:13:32 +0400

    added file task

---
+++ FrontoBack-ConnectSql-task/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontoBack_ConnectSql_task.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
